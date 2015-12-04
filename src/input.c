59a344145b267b1499cdc4bf03dbb08160b54c4c
Anders Eriksson <aeri3@kth.se>
2015-12-04 14:34:23 +0100

    Added input handling for buttons 1, 2, 3 and 4.

---
+++ src/input.c
@@ -0,0 +1,69 @@
+/*
+ * input.c handles all input including buttons
+ * used for controlling the game.
+ *
+ * Buttons 1-4 on the chipKIT Basic I/O Shield:
+ * BTN   PIN     PIC32sign Bitrange
+ * 1     4       RF1       17/1
+ * 2     34      RD5       21/5
+ * 3     36      RD6       22/6
+ * 4     37      RD7       23/7
+ */
+
+#include "input.h"
+#include "pic32mx.h"
+
+#define COOLDOWN 1000
+
+struct button {
+        int cooldown;
+        int (*port)(int);
+        int shift;
+};
+
+int get_portf(int shift) {
+        return (PORTF >> shift) & 1;
+}
+
+int get_portd(int shift) {
+        return (PORTD >> shift) & 1;
+}
+
+static struct button buttons[4];
+
+void inputinit(void)
+{
+        TRISFSET = 0x1;  /* BTN1 */
+        TRISDSET = 0x70; /* BTN2, BTN3, and BTN4 */
+
+        /* Create struct for BTN 1 */
+        struct button btn_one = {
+                .cooldown = COOLDOWN,
+                .port = &get_portf,
+                .shift = 1
+        };
+        buttons[0] = btn_one;
+
+        /* Create struct for BTN1, BTN2, and BTN3 */
+        for (int i = 1; i < 4; i++) {
+                struct button btn = {
+                        .cooldown = COOLDOWN,
+                        .port = &get_portd,
+                        .shift = i+4
+                };
+                buttons[i] = btn;
+        }
+}
+
+int* inputloop(void)
+{
+        int arr[4];
+        for (int i = 0; i < sizeof(buttons); i++) {
+                struct button btn = buttons[i];
+                int ispressed = btn.port(btn.shift);
+                btn.cooldown = !btn.cooldown && ispressed ? 
+                        COOLDOWN : btn.cooldown - 1;              
+                arr[i] = ispressed;
+        }
+        return arr;
+}
