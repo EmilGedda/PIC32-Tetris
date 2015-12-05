bcb9497b3303378da5a92e2cba2a58c377f8f9d5
Emil Gedda <emil.gedda@emilgedda.se>
2015-12-05 20:52:22 +0100

    Added header-file

---
+++ src/game.h
@@ -0,0 +1,8 @@
+#ifndef GAME_H
+#define GAME_H
+
+void run(void);
+
+void on_tick(void);
+
+#endif
--- src/main.c
+++ src/main.c
@@ -1,9 +1,16 @@
+#include "game.h"
+#include <pic32mx.h>
+#include "display.h"
+
 int main(void)
 {
-        for(;;);
+	run();
 }
 
 void user_isr(void)
 {
-
+	if (!(IFS(0) & 0x100)) return;	/* Poll time-out event flag */
+    	IFS(0) &= ~0x100;		/* Clear IFS */
+	on_tick();
+	display_update();
 }
